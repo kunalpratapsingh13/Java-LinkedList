ce605ed77d7378f4e554a6c7356d1448592a1add
kunalpratapsingh13 <40957858+kunalpratapsingh13@users.noreply.github.com>
2020-03-26 13:49:46 +0530

    Add files via upload

---
+++ LL_As_Stack.java
@@ -0,0 +1,33 @@
+package LinkedList;
+
+public class LL_As_Stack {
+	private LinkedList stack;
+	
+	public LL_As_Stack() {
+		this.stack = new LinkedList();
+	}
+	
+	public int size() {
+		return this.stack.size();
+	}
+	
+	public boolean isEmpty() {
+		return this.stack.isEmpty();
+	}
+	
+	public void push(int data) {
+		this.stack.addFirst(data);
+	}
+	
+	public int pop() throws Exception {
+		return this.stack.removeFirst();
+	}
+	
+	public int top() throws Exception {
+		return this.stack.getFirst();
+	}
+	
+	public void display() throws Exception {
+		this.stack.display();
+	}
+}
---
+++ LL_As_Stack_Client.java
@@ -0,0 +1,11 @@
+package LinkedList;
+
+public class LL_As_Stack_Client {
+	public static void main(String[]args) throws Exception {
+		LL_As_Stack stack = new LL_As_Stack();
+		stack.push(10);
+		stack.push(20);
+		stack.push(30);
+		stack.display();
+	}
+}
